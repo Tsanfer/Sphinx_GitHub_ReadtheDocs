1dadb67080922af044725b9370327db532b8afa0
Tsanfer <39004010+Tsanfer@users.noreply.github.com>
2020-02-19 19:50:04 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Tsanfer'
 author = 'Tsanfer'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2'
+release = '0.9'
 
 
 # -- General configuration ---------------------------------------------------
